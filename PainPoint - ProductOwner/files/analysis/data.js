5ed9fd8fc130f0790eb04bed106f52a2b9b0c073
= <=>
2018-05-24 14:05:21 +0700

    Deleted all files

--- PainPoint - ProductOwner/files/analysis/data.js
+++
@@ -1,7 +0,0 @@
-﻿$axure.loadCurrentPage(
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,[j,k,l,m,n,o,p,q,r,s],t,_(u,v,w,x,y,z,A,_(),B,_(C,D,E,F,G,_(H,I,J,K),L,null,M,F,N,F,O,P,Q,null,R,S,T,U,V,W,X,S),Y,_(),Z,_(),ba,_(bb,[])),bc,_(),bd,_());}; 
-var b="url",c="analysis.html",d="generationDate",e=new Date(1527142082042.46),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="variables",j="OnLoadVariable",k="SliderValue",l="Submitter",m="BusinessUnit",n="FirstName",o="SecondName",p="Country",q="City",r="CompanyName",s="OpenAddNewPP",t="page",u="packageId",v="b24c08dd38bf4c6d882283aaf8d71de3",w="type",x="Axure:Page",y="name",z="Analysis",A="notes",B="style",C="baseStyle",D="627587b6038d43cca051c114ac41ad32",E="pageAlignment",F="near",G="fill",H="fillType",I="solid",J="color",K=0xFFFFFFFF,L="image",M="imageHorizontalAlignment",N="imageVerticalAlignment",O="imageRepeat",P="auto",Q="favicon",R="sketchFactor",S="0",T="colorStyle",U="appliedColor",V="fontName",W="Applied Font",X="borderWidth",Y="adaptiveStyles",Z="interactionMap",ba="diagram",bb="objects",bc="masters",bd="objectPaths";
-return _creator();
-})());
